b9344fc80145c10993e0894a319b18f0816e4132
shekharmnnit <shekhark221@gmail.com>
2023-11-06 14:15:06 -0500

    test dataset

--- test.py
+++ test.py
@@ -6,8 +6,9 @@ import seaborn as sns
 trainDatasetfilePath = 'https://raw.githubusercontent.com/shekharmnnit/ML/main/Customer%20Churn%20Dataset/customer_churn_dataset-training-master.csv'
 testDatasetPath= 'https://raw.githubusercontent.com/shekharmnnit/ML/main/Customer%20Churn%20Dataset/customer_churn_dataset-testing-master.csv'
 training_df = pd.read_csv(trainDatasetfilePath)
+testing_df = pd.read_csv(testDatasetPath)
 print(training_df.head(5).to_string())
 print(training_df.isna().sum())
 
-print(training_df.head(5).to_string())
-print(training_df.isna().sum())
+print(testing_df.head(5).to_string())
+print(testing_df.isna().sum())
