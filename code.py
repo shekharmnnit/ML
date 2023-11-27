97e275768220f3f1b330ccd92c1db9dfe1981f40
shekharmnnit <shekhark221@gmail.com>
2023-11-27 00:40:03 -0500

    notebook code

--- code.py
+++ code.py
@@ -3,11 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-trainDatasetfilePath = 'https://raw.githubusercontent.com/shekharmnnit/ML/main/Customer%20Churn%20Dataset/customer_churn_dataset-training-master.csv'
-testDatasetPath= 'https://raw.githubusercontent.com/shekharmnnit/ML/main/Customer%20Churn%20Dataset/customer_churn_dataset-testing-master.csv'
-training_df = pd.read_csv(trainDatasetfilePath)
-print(training_df.head(5).to_string())
-print(training_df.isna().sum())
+# trainDatasetfilePath = 'https://raw.githubusercontent.com/shekharmnnit/ML/main/Customer%20Churn%20Dataset/customer_churn_dataset-training-master.csv'
+# testDatasetPath= 'https://raw.githubusercontent.com/shekharmnnit/ML/main/Customer%20Churn%20Dataset/customer_churn_dataset-testing-master.csv'
+# training_df = pd.read_csv(trainDatasetfilePath)
 
-print(training_df.head(5).to_string())
-print(training_df.isna().sum())
