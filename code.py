45890b3bb447d1a2f7c44e157c92a667c403821d
shekharmnnit <shekhark221@gmail.com>
2023-12-12 16:06:07 -0500

    del file

--- code.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
-
-# trainDatasetfilePath = 'https://raw.githubusercontent.com/shekharmnnit/ML/main/Customer%20Churn%20Dataset/customer_churn_dataset-training-master.csv'
-# testDatasetPath= 'https://raw.githubusercontent.com/shekharmnnit/ML/main/Customer%20Churn%20Dataset/customer_churn_dataset-testing-master.csv'
-# training_df = pd.read_csv(trainDatasetfilePath)
-
